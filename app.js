de9a8ac29fd1b56a2f3ab93c645fadfc5f7f8913
Michael Passade <michael.passade@codeimmersives.com>
2020-07-14 13:02:02 -0400

    setup directories and started designing home.ejs

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const mongoose = require('mongoose')
+// const flash = require('connect-flash')
+const morgan = require('morgan')
+const session = require('express-session')
+const passport = require('passport')
+const MongoStore = require('connect-mongo')(session)
+
+require('dotenv').config()
+require('./lib/passport.js')
+
+const userRouter = require('./routes/userRoutes.js')
+const indexRouter = require('./routes/index.js')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.SESSION_SECRET,
+        store: new MongoStore({
+            url: process.env.MONGODB_URI,
+            mongooseConnection: mongoose.connection,
+            autoReconnect: true
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 1000 * 60 * 60 * 24
+        }
+    })
+)
+
+// app.use(flash())
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+    res.locals.user = req.user
+    // res.locals.errors = req.flash('errors')
+    next()
+})
+
+app.use('/', indexRouter)
+app.use('/v1', userRouter)
+
+const port = process.env.PORT || 8000
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB Connected')
+}).catch(err => {
+  console.log(`MongoDB Error: ${err}`)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
